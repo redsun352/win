50d47b490c2b878dbd1fdd9daedb4521fdd08c6e
redsun352 <greedyy3843@gmail.com>
2026-09-02 23:20:57 +0300

    Add AutoInput TouchpadView patch source

---
+++ patches/TouchpadView.java
@@ -0,0 +1,10 @@
+package com.winlator.widget;
+
+import android.view.InputDevice;
+import android.view.MotionEvent;
+
+import com.winlator.inputcontrols.ExternalInputDeviceManager;
+
+/* AutoInput patch source. This file is overlaid onto the matching Winlator 11.1
+   source by GitHub Actions. */
+
